581906cd3cc42f32b43c85922ecee459620f7156
gustinjm <jean-marc.gustin@waterloo.eu>
2018-05-31 15:08:57 +0200

    Uses  custom iconUrl

--- src/app/mock-xhr-backend.ts
+++ src/app/mock-xhr-backend.ts
@@ -37,29 +37,33 @@ export class MockXHRBackend {
     id: 67,
     lat: 50.713600296946,
     lng: 4.399800458722,
-    description: 'passage pour pi�tons rue du gaz',
-    category: "orange"
+    description: 'Passage pour piétons rue du Gaz',
+    category: 'orange',
+    iconUrl: '/assets/orange-dot.png'
   },
   {
     id: 61,
     lat: 50.71173294994551,
     lng: 4.398852645368947,
     description: 'Trottoir Boulevard H. Rolin, en face du Delhaize',
-    category: "yellow"
+    category: 'red',
+    iconUrl: '/assets/red-dot.png'
   },
   {
     id: 55,  
     lat: 50.7069934,
     lng: 4.3714322,
     description: 'Chemin des Postes (entre la rue St Germain et la rue des Piles)',
-    category: "orange"
+    category: 'orange',
+    iconUrl: '/assets/orange-dot.png'
   },
   {
     id: 1,  
     lat: 50.72170274313,
     lng: 4.3845587142334,
     description: 'Au croisement de la drève du Garde et du Sentier de Rhode-St-Genèse',
-    category: "green"
+    category: 'green',
+    iconUrl: '/assets/green-dot.png'
   }
   ];
 }
